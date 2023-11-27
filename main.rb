7a7e97f359ed3752a17b6182a1da0b5341861bd7
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-27 19:53:17 +0100

    Define merge sort method

--- main.rb
+++ main.rb
@@ -14,3 +14,26 @@ def fibs_rec(num)
   fib_array << fib_array[-1] + fib_array[-2] if fib_array.length < num
   fib_array
 end
+
+def merge_sort(array)
+  return array if array.length == 1
+
+  length = array.length
+  left_array = merge_sort(array[0, length / 2])
+  right_array = merge_sort(array[length / 2, length])
+  merge_array(left_array, right_array)
+end
+
+def merge_array(arr1, arr2, array = [])
+  return array.concat arr1 if arr2.empty?
+  return array.concat arr2 if arr1.empty?
+
+  min = [arr1[0], arr2[0]].min
+  array << min
+  if arr1[0] == min
+    arr1.shift
+  else
+    arr2.shift
+  end
+  merge_array(arr1, arr2, array)
+end
