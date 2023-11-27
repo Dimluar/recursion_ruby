0bbd0106a580b050d2787cd1a3dd757f5ba98358
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-27 18:46:56 +0100

    Define recursive fibonacci

--- main.rb
+++ main.rb
@@ -5,3 +5,12 @@ def fibs(num)
   fib_array << fib_array[-1] + fib_array[-2] until fib_array.length == num
   fib_array
 end
+
+def fibs_rec(num)
+  return [0] if num.zero?
+  return [0, 1] if num == 1
+
+  fib_array = fibs_rec(num - 1)
+  fib_array << fib_array[-1] + fib_array[-2] if fib_array.length < num
+  fib_array
+end
